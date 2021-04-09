9a8f51844b0b3408a9d1e4ba28d57c4b94358c6d
Álvaro Galisteo <8809764+SrGMC@users.noreply.github.com>
2021-04-09 16:09:08 +0200

    Improved docker build script

--- docker/README.md
+++ docker/README.md
@@ -13,9 +13,42 @@ Run
 bash build.sh
 ```
 
+```
+USAGE:
+    ./build.sh [FLAGS] [OPTIONS]
+
+OPTIONS:
+    -t=, --tag=        Tag to add to all images.
+                       Example: {username}/image_name:{tag}
+    -u=, --username=   Username to add to all images
+                       Example: {username}/image_name:{tag}
+    -ag                Build SilverStrike image
+    -dd                Build DevDocs image
+    -np                Build Node.js + Python image
+    -sl                Build Shlink image
+    -sn                Build Shynet image
+
+FLAGS:
+
+    --help, -h         Show this help
+
+NOTES:
+    If no image option is provided, nothing will be built.
+```
+
+### Example
+
+```
+./build.sh -t=amd64 -u=srgmc -dd -np
+```
+
+This will build the DevDocs and Node.js + Python images as `srgmc/devdocs:amd64` and `srgmc/python-nodejs:amd64` respectively.
+
 ## Images
 
+- [SilverStrike](https://github.com/agstrike/silverstrike)
+- [DevDocs](https://github.com/freeCodeCamp/devdocs)
 - [docker-python-nodejs](https://github.com/nikolaik/docker-python-nodejs)
   - Node.js 12 and Python 3.8 are installed
-- [SilverStrike](https://github.com/agstrike/silverstrike)
 - [Shlink](https://github.com/shlinkio/shlink)
+- [Shynet](https://github.com/milesmcc/shynet)
--- docker/build.sh
+++ docker/build.sh
@@ -1,36 +1,131 @@
 #!/bin/bash
-mkdir node-python
-cp Dockerfiles/node-python.Dockerfile node-python/Dockerfile
-cd node-python
-sudo docker build . -t srgmc/python-nodejs:n12-p3.9
+#
+# docker
+# Script to create docker images
+#
+# Author: Álvaro Galisteo (https://alvaro.ga)
+# Copyright 2020 - GPLv3
 
-cd ..
+function help {
+    echo -e "$(tput bold)docker$(tput sgr 0)"
+    echo -e "Script to create docker images"
+    echo -e ""
+    echo -e "USAGE:"
+    echo -e "    ./build.sh [FLAGS] [OPTIONS]"
+    echo -e ""
+    echo -e "OPTIONS:"
+    echo -e "    -t=, --tag=        Tag to add to all images."
+    echo -e "                       Example: {username}/image_name:{tag}"
+    echo -e "    -u=, --username=   Username to add to all images"
+    echo -e "                       Example: {username}/image_name:{tag}"
+    echo -e "    -ag                Build SilverStrike image"
+    echo -e "    -dd                Build DevDocs image"
+    echo -e "    -np                Build Node.js + Python image"
+    echo -e "    -sl                Build Shlink image"
+    echo -e "    -sn                Build Shynet image"
+    echo -e ""
+    echo -e "FLAGS:\n"
+    echo -e "    --help, -h         Show this help"
+    echo -e ""
+    echo -e "NOTES:"
+    echo -e "    If no image option is provided, nothing will be built."
+}
 
-git clone https://github.com/agstrike/docker
-mv docker silverstrike
-cp Dockerfiles/silverstrike.Dockerfile silverstrike/Dockerfile
-cd silverstrike
-sudo docker build . -t srgmc/silverstrike:armv7l
+# Parse CLI arguments
+for i in "$@"; do
+    case $i in
+    	--help|-h)
+			help ; exit ;;
+	    -t=*|--tag=*)
+	    	TAG="${i#*=}";;
+	    -u=*|--username=*)
+	    	USERNAME="${i#*=}";;
+        -ag)
+            SILVERSTRIKE="1" ;;
+        -dd)
+            DEVDOCS="1" ;;
+        -np)
+            NODE_PYTHON="1";;
+        -sl)
+            SHLINK="1" ;;
+        -sn)
+            SHYNET="1" ;;
+        *)
+            echo -e "error: Unrecognized parameter $1\n"; help; exit 1;;
+    esac
+done
 
-cd ..
+if [[ $TAG == '' ]]; then
+	echo "error: TAG is not set"
+	echo ""
+	help
+	exit 1
+fi
 
-git clone https://github.com/shlinkio/shlink
-cd shlink
-sudo docker build . -t srgmc/shlink:armv7l
+if [[ $USERNAME == '' ]]; then
+	echo "error: USERNAME is not set"
+	echo ""
+	help
+	exit 1
+fi
 
-cd ..
+function buildSilverstrike {
+	git clone https://github.com/agstrike/docker
+	mv docker silverstrike
+	cp Dockerfiles/silverstrike.Dockerfile silverstrike/Dockerfile
+	cd silverstrike
+	sudo docker build . -t $USERNAME/silverstrike:$TAG
+}
 
-git clone https://github.com/milesmcc/shynet
-cd shynet
-sudo docker build . -t srgmc/shynet:armv7l
+function buildDevDocs {
+	git clone https://github.com/freeCodeCamp/devdocs.git
+	cp Dockerfiles/devdocs.Dockerfile devdocs/Dockerfile
+	cd devdocs
+	sudo docker build . -t $USERNAME/devdocs:$TAG
+}
 
-cd ..
+function buildNodePython {
+	mkdir node-python
+	cp Dockerfiles/node-python.Dockerfile node-python/Dockerfile
+	cd node-python
+	sudo docker build . -t $USERNAME/python-nodejs:$TAG
+}
 
-git clone https://github.com/freeCodeCamp/devdocs.git
-cp Dockerfiles/devdocs.Dockerfile devdocs/Dockerfile
-cd devdocs
-sudo docker build . -t srgmc/devdocs:armv7l
+function buildShlink {
+	git clone https://github.com/shlinkio/shlink
+	cd shlink
+	sudo docker build . -t $USERNAME/shlink:$TAG
+}
 
-cd ..
+function buildShynet {
+	git clone https://github.com/milesmcc/shynet
+	cd shynet
+	sudo docker build . -t $USERNAME/shynet:$TAG
+}
 
-docker build -t plexinc/pms-docker:latest -f Dockerfile.armv7 .
+# Main logic
+if [[ $SILVERSTRIKE -eq 1 ]]; then
+	(buildSilverstrike)
+	rm -rf silverstrike
+fi
+
+if [[ $DEVDOCS -eq 1 ]]; then
+	(buildDevDocs)
+	rm -rf devdocs
+fi
+
+
+if [[ $NODE_PYTHON -eq 1 ]]; then
+	(buildNodePython)
+	rm -rf node-python
+fi
+
+if [[ $SHLINK -eq 1 ]]; then
+	(buildShlink)
+	rm -rf shlink
+fi
+
+if [[ $SHYNET -eq 1 ]]; then
+	(buildShynet)
+	rm -rf shynet
+fi
